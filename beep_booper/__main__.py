4a4f7705cac6e8810644d01545a9773863b7107c
PerpetualPossum <140846346+PerpetualPossum@users.noreply.github.com>
2024-02-25 14:48:47 -0600

    simple bot start

---
+++ beep_booper/__main__.py
@@ -0,0 +1,15 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+client.run(os.getenv("DISCORD_TOKEN", ""))
